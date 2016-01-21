a3c52e70dbc274fcff9259d76ad57f65d4cb981c
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2016-01-21 13:01:58 +0530

    Login/Signup View

--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -10,29 +10,44 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
-
+    
+    var signupActive = true
+    
     @IBOutlet var iUserName: UITextField!
     
     @IBOutlet var iPassword: UITextField!
-   
+    
+    @IBOutlet var signupButton: UIButton!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    @IBOutlet var registrationLabel: UILabel!
+    
+    
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    func displayAlert( title: String, message: String){
+        
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
     
     @IBAction func signUp(sender: AnyObject) {
         
         //checking if username and password are entered
-        
         if iUserName.text == "" || iPassword == "" {
             
-            let alert = UIAlertController(title: "Error in Form", message: "Enter a Username and Password", preferredStyle: UIAlertControllerStyle.Alert)
+            displayAlert("Error in Form", message: "Enter a Username and Password")
             
-            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
-                
-                self.dismissViewControllerAnimated(true, completion: nil)
-                
-            }))
-            
-            self.presentViewController(alert, animated: true, completion: nil)
-        
         } else {
             
             activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
@@ -44,48 +59,109 @@ class ViewController: UIViewController {
             
             UIApplication.sharedApplication().beginIgnoringInteractionEvents()
             
-            var user = PFUser()
-            
-            user.username = iUserName.text
-            user.password = iPassword.text
-            
-            user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+            if signupActive == true {
                 
-                self.activityIndicator.stopAnimating()
-                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                let user = PFUser()
                 
-                if error == nil {
-                    
-                    
-                    
-                }
+                user.username = iUserName.text
+                user.password = iPassword.text
                 
-            })
-            
+                var errorMessage = "Please Try Later"
+                
+                user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    if error == nil {
+                        
+                        //SignUp Successful
+                        
+                    } else {
+                        
+                        if let errorString = error!.userInfo["error"] as? String {
+                            
+                            errorMessage = errorString
+                            
+                            self.displayAlert("Failed Sign Up", message: errorMessage)
+                            
+                        }
+                        
+                    }
+                    
+                })
+                
+            } else {
+                
+                PFUser.logInWithUsernameInBackground(iUserName.text!, password: iPassword.text!, block: { (user, error) -> Void in
+                    
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    
+                    if user != nil {
+                        
+                        //Logged In!
+                        
+                    } else {
+                        
+                        var errorMessage = "Please Try Later"
+                        
+                        if let errorString = error!.userInfo["error"] as? String {
+                            
+                            errorMessage = errorString
+                            
+                            self.displayAlert("Failed Login", message: errorMessage)
+                        }
+                        
+                    }
+                    
+                })
+                
+            }
             
         }
         
     }
- 
+    
     @IBAction func logIn(sender: AnyObject) {
         
-        
+        if signupActive == true {
+            
+            signupButton.setTitle("Login", forState: UIControlState.Normal)
+            
+            registrationLabel.text = "New to the app?"
+            
+            loginButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            
+            signupActive = false
+            
+        } else {
+            
+            signupButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            
+            registrationLabel.text = "Returning Back?"
+            
+            loginButton.setTitle("Login", forState: UIControlState.Normal)
+            
+            signupActive = true
+            
+        }
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-    
-    
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
