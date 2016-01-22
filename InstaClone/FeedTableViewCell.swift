f58f3ff1e40f3a80f34deb63b2804a1e1cf98fa1
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2016-01-22 18:24:33 +0530

    Basic Functionality

---
+++ InstaClone/FeedTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FeedTableViewCell.swift
+//  InstaClone
+//
+//  Created by Aditya Vikram Godawat on 22/01/16.
+//  Copyright © 2016 Wow Labz. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    
+    @IBOutlet var postedImage: UIImageView!
+    
+    @IBOutlet var username: UILabel!
+    
+    @IBOutlet var message: UILabel!
+  
+    
+    
+}
